c3ce8287e2d8437ba697a6147e2edac4098b5db2
EunseokGo <gomonk3037@gmail.com>
2018-05-31 14:12:37 +0900

    Attach layout to App componet

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import DevTools from 'mobx-react-devtools';
 
 import Routes from './Routes';
+import { Header, Body, Footer } from './components/Layout';
 
 class App extends Component {
   render() {
     return (
       <Router history={history}>
-        <div id="App">
-          <Routes />
+        <div id="app">
+          <Header />
+          <Body>
+            <Routes />
+          </Body>
+          <Footer />
           {process.env.NODE_ENV !== 'production' && <DevTools />}
         </div>
       </Router>
