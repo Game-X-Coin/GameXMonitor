6ac9df885900700dc2ed74a64521acee72872866
EunseokGo <gomonk3037@gmail.com>
2018-05-31 15:23:42 +0900

    Implement pages with JumboTron

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import { JumboTron } from '../components/Layout';
 
 class NotFoundPage extends Component {
   render() {
-    return <div>404</div>;
+    return (
+      <div>
+        <JumboTron>Not Found :(</JumboTron>
+        <div className="container">
+          <h2 className="mb-3">
+            This is not the web page you are looking for...
+          </h2>
+          <h4>
+            I'll give you a <Link to="/">link</Link> to go back.
+          </h4>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,8 +1,65 @@
 import React, { Component } from 'react';
 
+import { JumboTron } from '../../components/Layout';
+import Table from '../../components/Table';
+
 class WelcomePage extends Component {
   render() {
-    return <div>welcome</div>;
+    return (
+      <div>
+        <JumboTron>Check GXC block and transaction information</JumboTron>
+        <div className="container">
+          <h4>Lastest Transactions</h4>
+          <Table
+            fetching={true}
+            renderHeader={() => (
+              <tr>
+                <th>ID</th>
+                <th>Timestamp</th>
+                <th>Sender</th>
+                <th>Recipent</th>
+                <th>Amount (GXC)</th>
+                <th>Fee (GXC)</th>
+              </tr>
+            )}
+            renderBody={() => (
+              <tr>
+                <th scope="row">16614400022228999619</th>
+                <td>1min ago</td>
+                <td>16614400022228999619</td>
+                <td>16614400022228999619</td>
+                <td>123,123</td>
+                <td>0.1</td>
+              </tr>
+            )}
+          />
+
+          <h4>Lastest Blocks</h4>
+          <Table
+            renderHeader={() => (
+              <tr>
+                <th>ID</th>
+                <th>Timestamp</th>
+                <th>Sender</th>
+                <th>Recipent</th>
+                <th>Amount (GXC)</th>
+                <th>Fee (GXC)</th>
+              </tr>
+            )}
+            renderBody={() => (
+              <tr>
+                <th scope="row">16614400022228999619</th>
+                <td>1min ago</td>
+                <td>16614400022228999619</td>
+                <td>16614400022228999619</td>
+                <td>123,123</td>
+                <td>0.1</td>
+              </tr>
+            )}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
