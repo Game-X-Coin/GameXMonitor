a18b3008ebba04cc96e9755eac0ef5590c857d65
EunseokGo <gomonk3037@gmail.com>
2018-06-11 00:57:39 +0900

    Add request utils working with 'axios'

---
+++ src/utils/request.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+
+const { API_ENDPOINT } = process.env;
+
+const defaultHeaders = {};
+
+const defaultOptions = {
+  baseURL: API_ENDPOINT,
+  method: 'get'
+};
+
+const request = options => {
+  let cancel;
+
+  const promise = new Promise(async (resolve, reject) => {
+    try {
+      const response = await axios.request({
+        ...defaultOptions,
+        ...options,
+        cancelToken: new axios.CancelToken(c => {
+          cancel = c;
+        }),
+        headers: {
+          ...defaultHeaders,
+          ...options.headers
+        }
+      });
+      resolve(response.data);
+    } catch (error) {
+      if (options.cancelable) {
+        reject('Request canceled');
+      } else {
+        reject(error.response.data);
+      }
+    }
+  });
+
+  if (options.cancelable) {
+    return {
+      promise,
+      cancel
+    };
+  }
+
+  return promise;
+};
+
+export { defaultHeaders as headers };
+export default request;
