01fa7158e636020a1aee7bda8953c2ab67f2db05
EunseokGo <gomonk3037@gmail.com>
2018-06-30 17:56:59 +0900

    Change api

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,52 @@
 import request from '../utils/request';
+import dataStore from '../stores/dataStore';
 
-export const blockAPI = {
-  list: params => request({ url: '/blocks', params }),
-  single: id => request({ url: `/blocks/${id}` })
+export const chainAPI = {
+  getInfo() {
+    return request({ url: `/chain/get_info` });
+  },
+
+  getBlock(block_num_or_id) {
+    return request({ url: `/chain/get_block`, data: { block_num_or_id } });
+  },
+
+  async getBlocks(page = 1, offset = 20) {
+    const max = dataStore.chain.head_block_num;
+    const min = 1;
+
+    const start = max - (page - 1) * offset;
+    const end = start - offset < min ? min : start - offset;
+
+    let promises = [];
+
+    for (let i = start; i >= end; i -= 1) {
+      promises.push(this.getBlock(i));
+    }
+
+    return await Promise.all(promises);
+  },
+
+  getAccount(account_name) {
+    return request({ url: '/chain/get_account', data: { account_name } });
+  },
+
+  getBalance(account) {
+    return request({
+      url: '/chain/get_currency_balance',
+      data: { code: 'eosio.token', account, symbol: 'EOS' }
+    });
+  }
 };
 
-export const transactionAPI = {
-  list: params => request({ url: '/transactions', params }),
-  single: id => request({ url: `/transactions/${id}` })
-};
+export const historyAPI = {
+  getTransaction(id) {
+    return request({ url: `/history/get_transaction`, data: { id } });
+  },
 
-export const addressAPI = {
-  list: params => request({ url: '/addresses', params }),
-  single: id => request({ url: `/addresses/${id}` })
+  getActions(account_name) {
+    return request({
+      url: '/history/get_actions',
+      data: { account_name, pos: -1, offset: -20 }
+    });
+  }
 };
--- src/stores/dataStore.js
+++ src/stores/dataStore.js
@@ -1,31 +1,139 @@
 import { observable, action, runInAction } from 'mobx';
 
-import { transactionAPI } from '../services/api';
+import { chainAPI, historyAPI } from '../services/api';
 import { request, pending, fulfilled, rejected } from '../utils/store/request';
 
 const requests = {
-  getTransactions: request
+  getChainInfo: request,
+  getBlocks: request,
+  getBlock: request,
+  getTransaction: request,
+  getAccount: request,
+  getActions: request
 };
 
 class dataStore {
-  @observable transactions = [];
+  @observable chain = {};
+
+  @observable blocks = [];
+  @observable block = {};
+
+  @observable transaction = {};
+
+  @observable account = {};
+
+  @observable actions = {};
 
   @observable requests = requests;
 
   @action
-  getTransactions = async () => {
-    this.requests.getTransactions = pending;
+  getChainInfo = async () => {
+    this.requests.getChainInfo = pending;
 
     try {
-      const data = await transactionAPI.list();
+      const data = await chainAPI.getInfo();
 
       runInAction(() => {
-        this.transactions = data;
-        this.requests.getTransactions = fulfilled;
+        this.chain = data;
+        this.requests.getChainInfo = fulfilled;
       });
     } catch (error) {
       runInAction(() => {
-        this.requests.getTransactions = rejected;
+        this.requests.getChainInfo = rejected;
+      });
+    }
+  };
+
+  @action
+  getBlocks = async (page, limit) => {
+    this.requests.getBlocks = pending;
+
+    try {
+      const data = await chainAPI.getBlocks(page, limit);
+
+      runInAction(() => {
+        this.blocks = data;
+        this.requests.getBlocks = fulfilled;
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.requests.getBlocks = rejected;
+      });
+    }
+  };
+
+  @action
+  getBlock = async id => {
+    this.requests.getBlock = pending;
+
+    try {
+      const data = await chainAPI.getBlock(id);
+
+      runInAction(() => {
+        this.block = data;
+        this.requests.getBlock = fulfilled;
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.requests.getBlock = rejected;
+      });
+    }
+  };
+
+  @action
+  getTransaction = async id => {
+    this.requests.getTransaction = pending;
+
+    try {
+      const data = await historyAPI.getTransaction(id);
+
+      runInAction(() => {
+        this.transaction = data;
+        this.requests.getTransaction = fulfilled;
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.requests.getTransaction = rejected;
+      });
+    }
+  };
+
+  @action
+  getAccount = async id => {
+    this.requests.getAccount = pending;
+
+    try {
+      const data = await chainAPI.getAccount(id);
+      const [balance] = await chainAPI.getBalance(id);
+
+      runInAction(() => {
+        this.account = {
+          ...data,
+          balance
+        };
+        this.requests.getAccount = fulfilled;
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.requests.getAccount = rejected;
+      });
+    }
+  };
+
+  @action
+  getActions = async id => {
+    this.requests.getActions = pending;
+
+    try {
+      const data = await historyAPI.getActions(id);
+
+      runInAction(() => {
+        this.actions = data;
+        this.requests.getActions = fulfilled;
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.requests.getActions = rejected;
       });
     }
   };
