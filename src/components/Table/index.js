c65d86cd8cffab1a1021ceacf3ec6902ecbe4f95
EunseokGo <gomonk3037@gmail.com>
2018-06-30 17:59:51 +0900

    Add table component

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,31 +1,18 @@
 import React from 'react';
 import { Table as BootstrapTable } from 'reactstrap';
-import LoadingSpinner from '../LoadingSpinner';
 
 import './style.scss';
 
 const Table = ({
   renderHeader = () => null,
   renderBody = () => null,
-  fetching = false
+  vertical = false
 }) => {
   return (
-    <div className="table-wrapper">
-      <BootstrapTable striped>
-        <thead>{renderHeader()}</thead>
-        <tbody>
-          {fetching ? (
-            <tr>
-              <td className="position-relative loading-row" colSpan="100">
-                <LoadingSpinner />
-              </td>
-            </tr>
-          ) : (
-            renderBody()
-          )}
-        </tbody>
-      </BootstrapTable>
-    </div>
+    <BootstrapTable className={vertical ? 'vertical' : ''} striped>
+      <thead>{renderHeader()}</thead>
+      <tbody>{renderBody()}</tbody>
+    </BootstrapTable>
   );
 };
 
