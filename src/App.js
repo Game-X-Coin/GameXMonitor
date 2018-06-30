bd77c84320b13ccb904daf862dbba0db2230754f
EunseokGo <gomonk3037@gmail.com>
2018-06-30 18:00:30 +0900

    Scroll top on router changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react';
 import { browserHistory as history } from 'react-router';
 import { hot } from 'react-hot-loader';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, withRouter } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 
 import Routes from './Routes';
 import { Header, Body, Footer } from './components/Layout';
 
+@withRouter
+class ScrollToTop extends Component {
+  componentDidUpdate(prevProps) {
+    if (prevProps.location !== this.props.location) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+
+@inject('dataStore')
+@observer
 class App extends Component {
+  componentDidMount() {
+    const { getChainInfo } = this.props.dataStore;
+
+    getChainInfo();
+
+    setInterval(() => {
+      getChainInfo();
+    }, 3000);
+  }
+
   render() {
     return (
       <Router history={history}>
-        <div id="app">
-          <Header />
-          <Body>
-            <Routes />
-          </Body>
-          <Footer />
-          {process.env.NODE_ENV !== 'production' && <DevTools />}
-        </div>
+        <ScrollToTop>
+          <div id="app">
+            <Header />
+            <Body>
+              <Routes />
+            </Body>
+            <Footer />
+            {process.env.NODE_ENV !== 'production' && <DevTools />}
+          </div>
+        </ScrollToTop>
       </Router>
     );
   }
