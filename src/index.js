283d2e52272825d88df32f315f50c03b3fed1815
EunseokGo <gomonk3037@gmail.com>
2018-07-05 19:55:44 +0900

    Fix GA

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactGA from 'react-ga';
 import { browserHistory as history } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import { configure } from 'mobx';
 import { Provider } from 'mobx-react';
-
-import { GA_ID } from './constants';
 
 import stores from './stores';
 
@@ -14,8 +11,6 @@ import App from './App';
 
 import 'bootstrap/scss/bootstrap.scss';
 import './styles/main.scss';
-
-ReactGA.initialize(GA_ID);
 
 configure({
   enforceActions: true
