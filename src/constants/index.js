b9fc4ec5d775139cc0e6dec26f1e23727d86239e
Go Eun-Seok <gomonk3037@gmail.com>
2018-12-20 14:09:39 +0900

    Store producers as constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,6 @@
 import { GA_ID } from './constant';
 import { routes } from './routes';
+import { producers } from './producers';
+import { images } from './images';
 
-export { GA_ID, routes };
+export { GA_ID, routes, producers, images };
